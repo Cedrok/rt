981d3289a51ce4a0aaa97b7a5fc355dac52f1550
Cedric VAUTRAIN <cvautrai@e1r8p1.42.fr>
2018-06-15 12:26:19 +0200

    prepa parser_get_objs.c, add test scene

--- srcs/parser_get_objs.c
+++ srcs/parser_get_objs.c
@@ -6,7 +6,7 @@
 /*   By: cvautrai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/06 15:03:41 by cvautrai          #+#    #+#             */
-/*   Updated: 2018/06/15 11:34:20 by cvautrai         ###   ########.fr       */
+/*   Updated: 2018/06/15 12:18:40 by cvautrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,6 +91,20 @@ static void	grab_cut(t_shape *obj, int *fd)
 	ft_strdel(&line);
 }
 
+static void	grab_texture_bis(t_shape *obj, char *line)
+{
+	if (!ft_strncmp(line, "\t\tx_offset:", 11))
+		obj->textunit.x_offset = ft_atof(line + 11);
+	if (!ft_strncmp(line, "\t\ty_offset:", 11))
+		obj->textunit.y_offset = ft_atof(line + 11);
+	if (!ft_strncmp(line, "\t\trainbow:", 10))
+		obj->textunit.has_rainbow = ft_atof(line + 10);
+	if (!ft_strncmp(line, "\t\tchecker:", 10))
+		obj->textunit.has_checker = ft_atof(line + 10);
+	if (!ft_strncmp(line, "\t\twaves:", 8))
+		obj->textunit.has_waves = ft_atof(line + 8);
+}
+
 static void	grab_texture(t_shape *obj, int *fd)
 {
 	char	*line;
@@ -108,16 +122,7 @@ static void	grab_texture(t_shape *obj, int *fd)
 			obj->textunit.x_scale = ft_atof(line + 10);
 		if (!ft_strncmp(line, "\t\ty_scale:", 10))
 			obj->textunit.y_scale = ft_atof(line + 10);
-		if (!ft_strncmp(line, "\t\tx_offset:", 11))
-			obj->textunit.x_offset = ft_atof(line + 11);
-		if (!ft_strncmp(line, "\t\ty_offset:", 11))
-			obj->textunit.y_offset = ft_atof(line + 11);
-		if (!ft_strncmp(line, "\t\trainbow:", 10))
-			obj->textunit.has_rainbow = ft_atof(line + 10);
-		if (!ft_strncmp(line, "\t\tchecker:", 10))
-			obj->textunit.has_checker = ft_atof(line + 10);
-		if (!ft_strncmp(line, "\t\twaves:", 8))
-			obj->textunit.has_waves = ft_atof(line + 8);
+		grab_texture_bis(obj, line);
 		if (!ft_strcmp(line, "}"))
 			ft_abort_free("no end to texture definition", line);
 	}
@@ -127,12 +132,48 @@ static void	grab_texture(t_shape *obj, int *fd)
 	ft_strdel(&line);
 }
 
+static void	grab_obj_ter(t_shape *obj, char *line)
+{
+	if (!ft_strncmp(line, "\theight:", 8))
+		obj->height = ft_atof(line + 8);
+	if (!ft_strncmp(line, "\twidth:", 7))
+		obj->width = ft_atof(line + 7);
+	if (!ft_strncmp(line, "\topacity:", 9))
+		obj->opacity = ft_atof(line + 9);
+	if (!ft_strncmp(line, "\tbrillance:", 11))
+		obj->brillance = ft_atof(line + 11) * 0.1;
+}
+
+static void	grab_obj_bis(t_shape *obj, char *line)
+{
+	t_vector3d	tmp;
+	char		*str_tmp;
+
+	if (!ft_strncmp(line, "\trot:", 5))
+	{
+		tmp = extract_vd3(line);
+		obj->rot = new_matrix(tmp.x, tmp.y, tmp.z);
+		obj->inv_rot = matrix_inv(obj->rot);
+	}
+	if (!ft_strncmp(line, "\tcolor:", 7))
+	{
+		str_tmp = extract_text(line);
+		obj->color = hex2int(str_tmp);
+		ft_strdel(&str_tmp);
+	}
+	if (!ft_strncmp(line, "\tradius:", 8))
+	{
+		if (obj->type == CONE)
+			obj->radius = ft_atof(line + 8) * DEG2RAD;
+		else
+			obj->radius = ft_atof(line + 8);
+	}
+}
+
 static void	grab_obj(t_scene *scene, int *fd)
 {
 	char		*line;
 	t_shape		obj;
-	t_vector3d	tmp;
-	char		*str_tmp;
 
 	obj = default_shape(-10);
 	line = ft_strnew(0);
@@ -145,37 +186,12 @@ static void	grab_obj(t_scene *scene, int *fd)
 			obj.type = shape_type(extract_text(line));
 		if (!ft_strncmp(line, "\tpos:", 5))
 			obj.origin = extract_vd3(line);
-		if (!ft_strncmp(line, "\trot:", 5))
-		{
-			tmp = extract_vd3(line);
-			obj.rot = new_matrix(tmp.x, tmp.y, tmp.z);
-			obj.inv_rot = matrix_inv(obj.rot);
-		}
-		if (!ft_strncmp(line, "\tcolor:", 7))
-		{
-			str_tmp = extract_text(line);
-			obj.color = hex2int(str_tmp);
-			ft_strdel(&str_tmp);
-		}
-		if (!ft_strncmp(line, "\tradius:", 8))
-		{
-			if (obj.type == CONE)
-				obj.radius = ft_atof(line + 8) * DEG2RAD;
-			else
-				obj.radius = ft_atof(line + 8);
-		}
-		if (!ft_strncmp(line, "\theight:", 8))
-			obj.height = ft_atof(line + 8);
-		if (!ft_strncmp(line, "\twidth:", 7))
-			obj.width = ft_atof(line + 7);
-		if (!ft_strncmp(line, "\tbrillance:", 11))
-			obj.brillance = ft_atof(line + 11) * 0.1;
-		if (!ft_strncmp(line, "\topacity:", 9))
-			obj.opacity = ft_atof(line + 9);
 		if (!ft_strcmp(line, "\ttexture{"))
 			grab_texture(&obj, fd);
 		if (!ft_strcmp(line, "\tcut{"))
 			grab_cut(&obj, fd);
+		grab_obj_bis(&obj, line);
+		grab_obj_ter(&obj, line);
 	}
 	ft_strdel(&line);
 	obj = check_obj(&obj);
