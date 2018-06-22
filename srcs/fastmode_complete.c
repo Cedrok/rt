0b8ed4ec48b14ae0864e63773f3c4192b2af1d8c
Pringles <Pringles@dump-pedago-2.42.fr>
2018-06-22 10:44:01 +0200

    black lines fixed

--- srcs/fastmode_complete.c
+++ srcs/fastmode_complete.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: Pringles <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/20 12:01:37 by Pringles          #+#    #+#             */
-/*   Updated: 2018/06/21 10:36:03 by tmilon           ###   ########.fr       */
+/*   Created: 2018/06/22 09:49:20 by Pringles          #+#    #+#             */
+/*   Updated: 2018/06/22 10:43:14 by Pringles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,24 @@
 #include <math.h>
 #include <pthread.h>
 
-static void	color_closest_point(t_all *param, t_point p)
+static void	color_closest_point(t_all *param, t_point p, int w)
 {
-	if (p.x < 3 && p.y < 3)
-		param->colorarray[p.x + p.y * param->env->surf->w] =
-			param->colorarray[p.x - (p.x % 5) + (p.y - p.y % 5)
-			* param->env->surf->w];
-	else if (p.x >= 3 && p.y < 3)
-		param->colorarray[p.x + p.y * param->env->surf->w] =
-			param->colorarray[
-			(int)fmin(p.x + 5 - (p.x % 5), param->env->surf->w - 1)
-			+ (p.y - p.y % 5)
-			* param->env->surf->w];
-	else if (p.x >= 3 && p.y >= 3)
-		param->colorarray[p.x + p.y * param->env->surf->w] =
-			param->colorarray[
-			(int)fmin(p.x + 5 - (p.x % 5), param->env->surf->w - 1)
-			+ (int)fmin((p.y + 5 - p.y % 5), param->maxy - 1)
-			* param->env->surf->w];
-	else if (p.x < 3 && p.y >= 3)
-		param->colorarray[p.x + p.y * param->env->surf->w] =
-			param->colorarray[
-			p.x - (p.x % 5) + (int)fmin(p.y + 5 - p.y % 5, param->maxy - 1)
-			* param->env->surf->w];
+	if (param->colorarray[p.x - (p.x % 5) + (p.y - p.y % 5)	* w])
+		param->colorarray[p.x + p.y * w] =
+			param->colorarray[p.x - (p.x % 5) + (p.y - p.y % 5) * w];
+	else if (param->colorarray[(int)fmin(p.x + 5 - (p.x % 5), w - 1)
+			+ (p.y - p.y % 5) * w])
+		param->colorarray[p.x + p.y * w] = param->colorarray[
+			(int)fmin(p.x + 5 - (p.x % 5), w - 1) + (p.y - p.y % 5) * w];
+	else if (param->colorarray[	(int)fmin(p.x + 5 - (p.x % 5), w - 1)
+			+ (int)fmin((p.y + 5 - p.y % 5), param->maxy - 1) * w])
+		param->colorarray[p.x + p.y * w] =
+			param->colorarray[(int)fmin(p.x + 5 - (p.x % 5), w - 1)
+			+ (int)fmin((p.y + 5 - p.y % 5), param->maxy - 1) * w];
+	else
+		param->colorarray[p.x + p.y * w] =
+			param->colorarray[p.x - (p.x % 5) +
+			(int)fmin(p.y + 5 - p.y % 5, param->maxy - 1) * w];
 }
 
 void		fastmode_complete(t_all *param)
@@ -48,10 +43,10 @@ void		fastmode_complete(t_all *param)
 	while (p.y < param->maxy)
 	{
 		p.x = -1;
-		if (p.y % 5 != 0)
-			while (++p.x < param->env->surf->w)
-				if (p.x % 5 != 0)
-					color_closest_point(param, p);
+			if (p.y % 5 != 0)
+				while (++p.x < param->env->surf->w)
+					if (p.x % 5 != 0)
+						color_closest_point(param, p, param->env->surf->w);
 		p.y++;
 	}
 }
