27ee388e713d5a619a303d73b7b3be467a5cfc49
Cedric VAUTRAIN <cvautrai@e1r7p2.42.fr>
2018-06-22 11:05:48 +0200

    quick norme fix

--- srcs/fastmode_complete.c
+++ srcs/fastmode_complete.c
@@ -6,7 +6,7 @@
 /*   By: Pringles <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/22 09:49:20 by Pringles          #+#    #+#             */
-/*   Updated: 2018/06/22 10:43:14 by Pringles         ###   ########.fr       */
+/*   Updated: 2018/06/22 10:55:59 by cvautrai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 static void	color_closest_point(t_all *param, t_point p, int w)
 {
-	if (param->colorarray[p.x - (p.x % 5) + (p.y - p.y % 5)	* w])
+	if (param->colorarray[p.x - (p.x % 5) + (p.y - p.y % 5) * w])
 		param->colorarray[p.x + p.y * w] =
 			param->colorarray[p.x - (p.x % 5) + (p.y - p.y % 5) * w];
 	else if (param->colorarray[(int)fmin(p.x + 5 - (p.x % 5), w - 1)
@@ -43,10 +43,10 @@ void		fastmode_complete(t_all *param)
 	while (p.y < param->maxy)
 	{
 		p.x = -1;
-			if (p.y % 5 != 0)
-				while (++p.x < param->env->surf->w)
-					if (p.x % 5 != 0)
-						color_closest_point(param, p, param->env->surf->w);
+		if (p.y % 5 != 0)
+			while (++p.x < param->env->surf->w)
+				if (p.x % 5 != 0)
+					color_closest_point(param, p, param->env->surf->w);
 		p.y++;
 	}
 }
