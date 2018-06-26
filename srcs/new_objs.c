0c19fb346253452b3dd10c038182f3e4886d228b
Benjamin SPINDLER <bspindle@e1r7p6.42.fr>
2018-06-26 08:34:55 +0200

    add light improved

--- srcs/new_objs.c
+++ srcs/new_objs.c
@@ -6,7 +6,7 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/15 11:04:58 by cvautrai          #+#    #+#             */
-/*   Updated: 2018/06/25 09:32:34 by bspindle         ###   ########.fr       */
+/*   Updated: 2018/06/26 08:34:37 by bspindle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,9 @@ void	new_spot(void *p, int c)
 	c = 0;
 	param = (t_all*)p;
 	new = default_light();
-	new.origin = param->scene.camera.origin;
-	new.origin.z += 5;
+	new.origin = vector_op(param->scene.camera.origin, lambda_product_d3(5,
+				adjust_direction(new_vector_3d(0, 0, 1),
+					param->scene.camera.rot)), '+');
 	ft_lstadd(&param->scene.light_lst, ft_lstnew(&new, sizeof(new)));
 	refresh_img(param);
 }
